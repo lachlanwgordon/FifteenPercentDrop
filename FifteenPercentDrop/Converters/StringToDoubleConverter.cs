dda3d2f1df0edc87d875333a49d0e156b4e17f97
Lachlan Gordon <lachlanwgordon@gmail.com>
2019-12-30 10:07:15 +1100

    Created a nullable doubel to string converter so that when you have no weight enters it shows blank
    this currently has an unnecessary entry showing on screen, will fix

--- FifteenPercentDrop/Converters/StringToDoubleConverter.cs
+++ FifteenPercentDrop/Converters/StringToDoubleConverter.cs
@@ -7,26 +7,37 @@ using Xamarin.Forms;
 namespace FifteenPercentDrop.Converters
 {
     [ValueConversion(typeof(string), typeof(double))]
-    public class StringToDoubleConverter : IValueConverter
+    public class StringToNullableDoubleConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (value is string == false)
+            if (value is double == false)
             {
-                return default(double);
+                return default(string);
             }
 
-            var input = (string)value;
-            var param = (object)parameter;
+            var input = (double?)value;
 
-            // TODO: Put your value conversion logic here.
+            //var parsed = Double.TryParse(input, out double result);
 
-            return default(double);
+            return input.ToString();
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if (value is string == false)
+            {
+                return default(double?);
+            }
+
+            var input = (string)value;
+
+            var parsed = Double.TryParse(input, out double result);
+
+            if (parsed)
+                return result;
+            return null;
+
         }
     }
 }
