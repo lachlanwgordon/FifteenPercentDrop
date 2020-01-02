537c4fd4e060e78fe76d89909c41329bccb45fb9
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-01-02 11:36:25 +1100

    Added about page and Debug Rainbows

--- FifteenPercentDrop/App.xaml.cs
+++ FifteenPercentDrop/App.xaml.cs
@@ -11,7 +11,25 @@ namespace FifteenPercentDrop
         public App()
         {
             InitializeComponent();
+            var rainbows = Xamarin.Essentials.Preferences.Get("Rainbows", false);
+            if (rainbows)
+            {
 
+                var DebugStyle = new Style(typeof(ContentPage))
+                {
+                    ApplyToDerivedTypes = true,
+                    Setters =
+                    {
+                        new Setter
+                        {
+                            Property = Xamarin.Forms.DebugRainbows.DebugRainbow.ShowColorsProperty,
+                            Value = true
+                        }
+                    }
+                };
+
+                Current.Resources.Add(DebugStyle);
+            }
             MainPage = new AppShell(); 
         }
 
---
+++ FifteenPercentDrop/Views/AboutPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FifteenPercentDrop.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+
+        void TextCell_Tapped(object sender, EventArgs e)
+        {
+        }
+
+
+        int clickCount;
+        async void DebugRainbows(object sender, EventArgs e)
+        {
+            clickCount++;
+            if(clickCount < 2)
+            {
+                await Task.Delay(500);
+                clickCount = 0;
+                return;
+            }
+            clickCount = 0;
+
+            var rainbows = Xamarin.Essentials.Preferences.Get("Rainbows", false);
+            Xamarin.Essentials.Preferences.Set("Rainbows", !rainbows);
+
+            if (Application.Current.Resources.ContainsKey("Xamarin.Forms.ContentPage"))
+                Application.Current.Resources.Remove("Xamarin.Forms.ContentPage");
+
+            var DebugStyle = new Style(typeof(ContentPage))
+            {
+                ApplyToDerivedTypes = true,
+                Setters =
+                    {
+                        new Setter
+                        {
+                            Property = Xamarin.Forms.DebugRainbows.DebugRainbow.ShowColorsProperty,
+                            Value = !rainbows
+                        }
+                    }
+            };
+
+            Application.Current.Resources.Add(DebugStyle);
+            DisplayAlert("You found the Debug Rainbow!", "Yay, Thanks Steven Thewissen", "Okay");
+        }
+
+
+    }
+}
