ff7ee80513643f2e48b9055ecbe86c6ea3e794f2
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-01-03 11:54:46 +1100

    Extracted weight entry into a custom control

--- FifteenPercentDrop/Views/CalculatorPage.xaml.cs
+++ FifteenPercentDrop/Views/CalculatorPage.xaml.cs
@@ -7,12 +7,6 @@ namespace FifteenPercentDrop.Views
 {
     public partial class CalculatorPage : ContentPage
     {
-        public void FocusWeightKeyboard(object sender, EventArgs e)
-        {
-            //VM.TotalWeight = null;
-            WeightEntry.Focus();
-        }
-
         public CalculatorPage()
         {
             InitializeComponent();
