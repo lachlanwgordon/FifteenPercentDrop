854952b8034815db5680195fbbc3065684310efb
Lachlan Gordon <lachlanwgordon@gmail.com>
2020-07-02 19:27:11 +1000

    Added new icon

--- FifteenPercentDrop.Android/MainActivity.cs
+++ FifteenPercentDrop.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace FifteenPercentDrop.Droid
 {
-    [Activity(Label = "FifteenPercentDrop", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "FifteenPercentDrop", Icon = "@mipmap/fifteenicon", RoundIcon = "@mipmap/fifteenicon_round", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
